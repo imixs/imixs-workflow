d4174df8ff6601c944f465d5a83789aecb15b24a
Ralph Soika <ralph.soika@imixs.com>
2014-04-26 13:14:12 +0200

    issue #31 - documentation

---
+++ imixs-workflow-core/src/main/java/org/imixs/workflow/test/WorkflowTestSuite.java
@@ -0,0 +1,174 @@
+package org.imixs.workflow.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.imixs.workflow.ItemCollection;
+import org.imixs.workflow.services.rest.RestClient;
+import org.imixs.workflow.xml.XMLItemCollectionAdapter;
+
+/**
+ * The Imixs WorkflowTestSuite provides a test framework for testing business
+ * logic. The test suite is based on the Imixs REST API and provides a set of
+ * REST clients to simulate and test different szenarios of a business process
+ * running in Imixs Workflow.
+ * 
+ * 
+ * The Imixs Test Suite can be used in a JUnit test class.
+ * 
+ * @author rsoika
+ * @version 1.0.0
+ */
+public class WorkflowTestSuite {
+	private static volatile WorkflowTestSuite instance = null;
+	private final static Logger logger = Logger
+			.getLogger(WorkflowTestSuite.class.getName());
+
+	Map<String, RestClient> clients = null;
+	String host = null;
+
+	// private constructor
+	private WorkflowTestSuite() {
+		clients = new HashMap<String, RestClient>();
+	}
+
+	/**
+	 * Singleton Lazy initialization
+	 * 
+	 * @return
+	 */
+	public static WorkflowTestSuite getInstance() {
+		if (instance == null) {
+			synchronized (WorkflowTestSuite.class) {
+				// Double check
+				if (instance == null) {
+					instance = new WorkflowTestSuite();
+				}
+			}
+		}
+		return instance;
+	}
+
+	/**
+	 * Server BASE URL
+	 */
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	/**
+	 * Public Logger method for test clients.
+	 * 
+	 * @param level
+	 * @param message
+	 */
+	public static void log(Level level, String message) {
+		logger.log(level, message);
+	}
+
+	/**
+	 * This method can be used to register a new test client. A test client is
+	 * identified by its userid. The test client can be used to perform
+	 * different methods.
+	 * 
+	 * @param user
+	 *            - user id
+	 * @param password
+	 *            - optional password
+	 */
+	public void joinParty(String user, String password) {
+		RestClient client = new RestClient();
+		if (password != null && !password.isEmpty()) {
+			client.setCredentials(user, password);
+		}
+		clients.put(user, client);
+	}
+
+	/**
+	 * Returns a RestClient instance for a specific user. The user must be
+	 * registered with the method joinParty.
+	 * 
+	 * @param user
+	 * @return
+	 */
+	public RestClient getClient(String user) {
+		return clients.get(user);
+	}
+
+	/**
+	 * Get the worklist for a specific user
+	 * 
+	 * @param user
+	 * @param resource - resource string 
+	 * @return
+	 * @see WorkflowSerice
+	 */
+	public List<ItemCollection> getWorklist(String user, String resource) {
+		List<ItemCollection> resultList = null;
+
+		RestClient client = clients.get(user);
+
+		try {
+			int result = client.get(getHost() + resource);
+			if (result >= 200 && result <= 299) {
+				String content = client.getContent();
+				resultList = XMLItemCollectionAdapter.readCollection(content
+						.getBytes());
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return resultList;
+	}
+
+	/**
+	 * Get the worklist for a specific user
+	 * 
+	 * @param user
+	 * @return
+	 */
+	public List<ItemCollection> getWorklist(String user) {
+		return getWorklist(user, "workflow/worklist.xml");
+	}
+
+	/**
+	 * post the workitem for a specific user
+	 * 
+	 * @param user
+	 * @return
+	 */
+	public ItemCollection processWorkitem(ItemCollection workitem, String user) {
+
+		ItemCollection resultWorkitem = null;
+		RestClient client = clients.get(user);
+
+		try {
+			int result = client.postEntity(getHost() + "workflow/workitem",
+					XMLItemCollectionAdapter.putItemCollection(workitem));
+			if (result >= 200 && result <= 299) {
+				String content = client.getContent();
+				resultWorkitem = XMLItemCollectionAdapter
+						.readItemCollection(content.getBytes());
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return resultWorkitem;
+	}
+
+}
