516e884bebe0f23d3f6ec8aa345288443b66edda
Ralph Soika <ralph.soika@imixs.com>
2014-05-09 17:05:36 +0200

    issue #35

--- imixs-workflow-faces/src/main/resources/META-INF/resources/imixs/imixs-faces.js
+++ imixs-workflow-faces/src/main/resources/META-INF/resources/imixs/imixs-faces.js
@@ -1,46 +1,88 @@
-
 // default jQuery date format
-var dateDisplayFormat="yy-mm-dd";
+var dateDisplayFormat = "yy-mm-dd";
 
 /*
- * This method converts a Java Date String into the jQuery format. 
- * See: http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+ * This method converts a Java Date String into the jQuery format. See:
+ * http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
  * http://docs.jquery.com/UI/Datepicker/formatDate
  * 
- * As jquery and javaDate format differs from each other we need to translate the JavaFormat
- * in a jquery understandable format.
- * Also the locale need to be set!
+ * As jquery and javaDate format differs from each other we need to translate
+ * the JavaFormat in a jquery understandable format. Also the locale need to be
+ * set!
  */
 function setDateFormat(javaDate) {
-	
+
 	// convert yyyy to yy,
-	// yy to y, 
-	if (javaDate.indexOf('yyyy')>-1) 
-		javaDate=javaDate.replace('yyyy','yy');
-	 else 
-		if (javaDate.indexOf('yy')>-1) 
-		 	javaDate=javaDate.replace('yy','y');
-	
-	// MMM to M, 
-	// MM to m, 
+	// yy to y,
+	if (javaDate.indexOf('yyyy') > -1)
+		javaDate = javaDate.replace('yyyy', 'yy');
+	else if (javaDate.indexOf('yy') > -1)
+		javaDate = javaDate.replace('yy', 'y');
+
+	// MMM to M,
+	// MM to m,
 	// M to m
-		if (javaDate.indexOf('MMMM')>-1) 
-		javaDate=javaDate.replace('MMMM','MM');
-	 else 
-		if (javaDate.indexOf('MMM')>-1) 
-		 	javaDate=javaDate.replace('MMM','M');
-		 else 
-			if (javaDate.indexOf('MM')>-1) 
-			 	javaDate=javaDate.replace('MM','mm');
-		 	else 
-				if (javaDate.indexOf('M')>-1) 
-			 		javaDate=javaDate.replace('M','m');	
-	
+	if (javaDate.indexOf('MMMM') > -1)
+		javaDate = javaDate.replace('MMMM', 'MM');
+	else if (javaDate.indexOf('MMM') > -1)
+		javaDate = javaDate.replace('MMM', 'M');
+	else if (javaDate.indexOf('MM') > -1)
+		javaDate = javaDate.replace('MM', 'mm');
+	else if (javaDate.indexOf('M') > -1)
+		javaDate = javaDate.replace('M', 'm');
+
 	// set jquery date format
-	dateDisplayFormat=javaDate;
+	dateDisplayFormat = javaDate;
 }
 
+/*
+ * Update the date time string for the imixs-datetime-picker....
+ */
+function getDateTimeInput(ainput) {
 
+	// find parent
+	datetimepicker = $(ainput).parent();
+
+	dateTimeInput = $(datetimepicker).children('[id$=imixsdatetimeinput]');
+	dateInput = $(datetimepicker).children('.imixs-date');
+	hourInput = $(datetimepicker).children('.imixs-time-hour');
+	minuteInput = $(datetimepicker).children('.imixs-time-minute');
+	timeString = $(dateInput).val();
+	if (hourInput.length && minuteInput.length) {
+		timeString = timeString + " " + $(hourInput).val() + ":"
+				+ $(minuteInput).val();
+	} else {
+		timeString = timeString + " 00:00";
+
+	}
+	$(dateTimeInput).val(timeString);
+}
+
+/*
+ * Update the calender inputs for the imixs-datetime-picker
+ */
+function setDateTimeInput(datetimepicker) {
+
+	dateTimeInput = $(datetimepicker).children('[id$=imixsdatetimeinput]');
+	dateInput = $(datetimepicker).children('.imixs-date');
+	hourInput = $(datetimepicker).children('.imixs-time-hour');
+	minuteInput = $(datetimepicker).children('.imixs-time-minute');
+
+	var timeString = $(dateTimeInput).val();
+	var timeValues = timeString.split(" ");
+
+	if (timeValues[0])
+		$(dateInput).val(timeValues[0]);
+
+	if (timeValues[1]) {
+		timeValues = timeValues[1].split(":");
+		if (timeValues[0] && hourInput.length)
+			$(hourInput).val(timeValues[0]);
+		if (timeValues[1] && minuteInput.length)
+			$(minuteInput).val(timeValues[1]);
+	}
+
+}
 
 /* This method styles input elements an imixs page elements */
 $.fn.imixsLayout = function(options) {
@@ -62,6 +104,26 @@ $.fn.imixsLayout = function(options) {
 			showOtherMonths : true,
 			selectOtherMonths : true,
 			dateFormat : dateDisplayFormat
+		});
+
+		// initialize datetime picker widget
+		$(".imixs-datetime-picker").each(function(index) {
+
+			// set current date value
+			setDateTimeInput($(this));
+
+			// on change events
+			$(this).children('.imixs-date').change(function() {
+				getDateTimeInput($(this));
+			});
+
+			$(this).children('.imixs-time-hour').change(function() {
+				getDateTimeInput($(this));
+			});
+
+			$(this).children('.imixs-time-minute').change(function() {
+				getDateTimeInput($(this));
+			});
 		});
 
 	});
@@ -131,27 +193,30 @@ $.fn.layoutImixsToggelPanel = function(options) {
 	});
 };
 
-
-
-
 $.fn.layoutImixsTooltip = function(options) {
 
 	return this.each(function() {
-		/* Imixs Tooltip support */		
-		// tested: flipfit none flipfit is not usefull for long content. better use flip
-		$( this ).prev().tooltip({ 
-			 position: {  my: "left top", at: "left+10 bottom", collision: "flip" },
-			 show: { duration: 800 },
-			 tooltipClass: "imixs-tooltip-content",
-			 content:  function() { 
-				var tooltip=$(this).next();
+		/* Imixs Tooltip support */
+		// tested: flipfit none flipfit is not usefull for long content. better
+		// use flip
+		$(this).prev().tooltip({
+			position : {
+				my : "left top",
+				at : "left+10 bottom",
+				collision : "flip"
+			},
+			show : {
+				duration : 800
+			},
+			tooltipClass : "imixs-tooltip-content",
+			content : function() {
+				var tooltip = $(this).next();
 				tooltip.hide();
-				return tooltip.html();  
-			} 
-			 
+				return tooltip.html();
+			}
 
-	 	});							
-		// hide all imixs tooltips 
-		$( this ).hide();
+		});
+		// hide all imixs tooltips
+		$(this).hide();
 	});
 };
