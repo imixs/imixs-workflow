e6a00cd26f8d5674632fc194c64e6f93660f2d1d
Ralph Soika <ralph.soika@imixs.com>
2015-09-07 22:46:18 +0200

    issue #93

--- imixs-workflow-core/src/main/java/org/imixs/workflow/xml/XMLIndexList.java
+++ imixs-workflow-core/src/main/java/org/imixs/workflow/xml/XMLIndexList.java
@@ -14,22 +14,22 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class XMLIndexList implements java.io.Serializable {
 	private static final long serialVersionUID = 1L;
-	private Map<String, Integer> map;
+	private Map<String, Integer> index;
 
 	public XMLIndexList() {
 
 	}
 
-	public XMLIndexList(Map<String, Integer> amap) {
+	public XMLIndexList(Map<String, Integer> map) {
 		super();
-		setMap(amap);
+		setMap(map);
 	}
 
 	public void setMap(Map<String, Integer> map) {
-		this.map = map;
+		this.index = map;
 	}
 
 	public Map<String, Integer> getMap() {
-		return map;
+		return index;
 	}
 }
