f9fb4cc7988cc65f248094fe905fdbef8cea8d7f
Ralph Soika <ralph.soika@imixs.com>
2016-04-13 09:43:50 +0200

    issue #152

--- imixs-workflow-core/src/main/java/org/imixs/workflow/util/XMLParser.java
+++ imixs-workflow-core/src/main/java/org/imixs/workflow/util/XMLParser.java
@@ -1,6 +1,8 @@
 package org.imixs.workflow.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,10 +15,9 @@ import java.util.regex.Pattern;
  */
 public class XMLParser {
 
-
 	/**
-	 * This method parses a xml tag for attributes. The method returns
-	 * a Map with all attributes found in the content string
+	 * This method parses a xml tag for attributes. The method returns a Map
+	 * with all attributes found in the content string
 	 * 
 	 * e.g. <item data="abc" value="def" />
 	 * 
@@ -25,12 +26,41 @@ public class XMLParser {
 	 * @param content
 	 * @return
 	 */
-	public static Map<String,String> parseAttributes(String content) {
-		Map<String,String> result=new HashMap<String,String>();
+	public static Map<String, String> findAttributes(String content) {
+		Map<String, String> result = new HashMap<String, String>();
 		Pattern p = Pattern.compile("(\\w+)=\"*((?<=\")[^\"]+(?=\")|([^\\s]+))\"*");
 		Matcher m = p.matcher(content);
-		while(m.find()){
+		while (m.find()) {
 			result.put(m.group(1), m.group(2));
+		}
+		return result;
+	}
+
+	/**
+	 * This method find specific tags inside a string and returns a list with
+	 * all tags.
+	 * 
+	 * e.g. <date field="abc" />
+	 * 
+	 * <date field="abc">def</date>
+	 * 
+	 * @param content
+	 * @return
+	 */
+	public static List<String> findTags(String content, String tag) {
+		List<String> result = new ArrayList<String>();
+
+		String regex = "<(?i)(" + tag + ")" + // matches the tag itself
+				"([^<]+)" + // then anything in between the opening and closing
+							// of the tag
+				"(</" + tag + ">|/>)"; // and finally the end tag corresponding
+										// to what we matched as the first group
+										// (Exony_Credit_Card_ID, tag1 or tag2)
+
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(content);
+		while (m.find()) {
+			result.add(m.group());
 		}
 		return result;
 	}
