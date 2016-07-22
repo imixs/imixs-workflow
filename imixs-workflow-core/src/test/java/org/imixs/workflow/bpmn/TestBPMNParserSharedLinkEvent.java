6560caf0968bb7689d4e18272a026dd7388b3e50
Ralph Soika <ralph.soika@imixs.com>
2016-07-22 15:10:32 +0200

    issue #174 - junit test added

---
+++ imixs-workflow-core/src/test/java/org/imixs/workflow/bpmn/TestBPMNParserSharedLinkEvent.java
@@ -0,0 +1,101 @@
+package org.imixs.workflow.bpmn;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import junit.framework.Assert;
+
+import org.imixs.workflow.ItemCollection;
+import org.imixs.workflow.exceptions.ModelException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+/**
+ * Test class test the Imixs BPMNParser.
+ * 
+ * This test verifies the linking an imixs-event with an imixs-task using a
+ * intermediate catch and intermediate throw link-event.
+ * 
+ * @author rsoika
+ */
+public class TestBPMNParserSharedLinkEvent {
+
+	@Before
+	public void setup() {
+
+	}
+
+	@After
+	public void teardown() {
+
+	}
+
+	/**
+	 * This test test intermediate link events and also loop throw events
+	 * @throws ParseException
+	 * @throws ParserConfigurationException
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws ModelException 
+	 */
+	@Test
+	public void testLinkEventSimple() throws ParseException, ParserConfigurationException, SAXException, IOException, ModelException {
+
+		InputStream inputStream = getClass().getResourceAsStream("/bpmn/shared-link-event.bpmn");
+
+		BPMNModel model = null;
+		try {
+			model = BPMNParser.parseModel(inputStream, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			Assert.fail();
+		} catch (ModelException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+		Assert.assertNotNull(model);
+
+		// Test Environment
+		Assert.assertTrue(model.getGroups().contains("Simple"));
+
+		// test count of elements
+		Assert.assertEquals(3, model.findAllTasks().size());
+
+		// test task 1000
+		ItemCollection task = model.getTask(1000 );
+		Assert.assertNotNull(task);
+		Assert.assertEquals("1.0.0", task.getItemValueString("$ModelVersion"));
+		Assert.assertEquals("Simple", task.getItemValueString("txtworkflowgroup"));
+
+		
+		Assert.assertEquals("Task Shared Link Event1", task.getItemValueString("txtName"));
+		
+		// test shared events
+		Assert.assertEquals(3,model.findAllEventsByTask(1000).size());
+		
+		ItemCollection event=model.getEvent(1000, 99);
+		Assert.assertEquals("cancel", event.getItemValueString("txtName"));
+		
+
+		
+		// test shared events
+		Assert.assertEquals(1,model.findAllEventsByTask(1100).size());
+
+
+	
+		Assert.assertEquals(0,model.findAllEventsByTask(1200).size());
+
+	}
+	
+	
+	
+	
+
+}
