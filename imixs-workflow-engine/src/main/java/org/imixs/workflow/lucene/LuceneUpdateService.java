9a242a88ffa4456f01607600739045537b2912aa
Ralph Soika <ralph.soika@imixs.com>
2016-08-26 22:22:56 +0200

    Updated Lucene Version to 6.2.0
    issue #192

--- imixs-workflow-engine/src/main/java/org/imixs/workflow/lucene/LuceneUpdateService.java
+++ imixs-workflow-engine/src/main/java/org/imixs/workflow/lucene/LuceneUpdateService.java
@@ -49,7 +49,6 @@ import org.apache.lucene.analysis.standard.ClassicAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field.Store;
 import org.apache.lucene.document.SortedDocValuesField;
-import org.apache.lucene.document.SortedSetDocValuesField;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.CorruptIndexException;
