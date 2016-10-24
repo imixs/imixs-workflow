b75d8146ccad262ba73faf02382da0115d2310ff
Ralph Soika <ralph.soika@imixs.com>
2016-10-24 23:52:10 +0200

    issue #220

--- imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/DocumentService.java
+++ imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/DocumentService.java
@@ -52,7 +52,6 @@ import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
-import javax.persistence.FlushModeType;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
