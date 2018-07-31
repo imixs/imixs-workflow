86fcbd07aff58af0fe3eae9302f52f3bd3c9ce95
Ralph Soika <ralph.soika@imixs.com>
2018-07-31 10:49:00 +0200

    prevent output of xml-declaration
    issue #404

--- imixs-workflow-core/src/main/java/org/imixs/workflow/util/XMLParser.java
+++ imixs-workflow-core/src/main/java/org/imixs/workflow/util/XMLParser.java
@@ -250,6 +250,7 @@ public class XMLParser {
 		DOMImplementationLS lsImpl = (DOMImplementationLS) node.getOwnerDocument().getImplementation().getFeature("LS",
 				"3.0");
 		LSSerializer lsSerializer = lsImpl.createLSSerializer();
+		lsSerializer.getDomConfig().setParameter("xml-declaration", false); 
 		NodeList childNodes = node.getChildNodes();
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < childNodes.getLength(); i++) {
