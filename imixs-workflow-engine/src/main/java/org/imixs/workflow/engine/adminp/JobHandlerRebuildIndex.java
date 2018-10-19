f7813fc61c10a3b74c00c67622e09c6007e806c4
Ralph Soika <ralph.soika@imixs.com>
2018-10-19 23:32:43 +0200

    implementation
    issue #435

--- imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/adminp/JobHandlerRebuildIndex.java
+++ imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/adminp/JobHandlerRebuildIndex.java
@@ -1,26 +1,23 @@
 package org.imixs.workflow.engine.adminp;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Logger;
 
-import javax.annotation.Resource;
 import javax.annotation.security.DeclareRoles;
 import javax.annotation.security.RunAs;
 import javax.ejb.EJB;
 import javax.ejb.LocalBean;
-import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.imixs.workflow.ItemCollection;
-import org.imixs.workflow.engine.DocumentService;
 import org.imixs.workflow.engine.jpa.Document;
 import org.imixs.workflow.engine.lucene.LuceneUpdateService;
 import org.imixs.workflow.exceptions.AccessDeniedException;
@@ -29,14 +26,7 @@ import org.imixs.workflow.exceptions.PluginException;
 /**
  * JobHandler to rebuild the lucene fulltext index.
  * 
- * A Job Document must provide the following information:
- * 
- * numIndex - start position
- * 
- * numBlockSize - documents to read during one run
- * 
- * 
- * 
+ * The job starts at 1970/01/01 and reads documents in sequence.
  * 
  * @author rsoika
  *
@@ -47,16 +37,13 @@ import org.imixs.workflow.exceptions.PluginException;
 @LocalBean
 public class JobHandlerRebuildIndex implements JobHandler {
 
-	private static final int DEFAULT_BLOCK_SIZE = 500;
+	private static final int BLOCK_SIZE = 500;
+	public final static String ITEM_SYNCPOINT = "syncpoint";
+	public final static String ITEM_SYNCDATE = "syncdate";
+	public static final String SNAPSHOT_TYPE_PRAFIX = "snapshot-";
 
 	@PersistenceContext(unitName = "org.imixs.workflow.jpa")
 	private EntityManager manager;
-
-	@Resource
-	SessionContext ctx;
-
-	@EJB
-	DocumentService documentService;
 
 	@EJB
 	LuceneUpdateService luceneUpdateService;
@@ -64,141 +51,152 @@ public class JobHandlerRebuildIndex implements JobHandler {
 	private static Logger logger = Logger.getLogger(JobHandlerRebuildIndex.class.getName());
 
 	/**
-	 * This method runs the RebuildLuceneIndexJob. The adminp job description
-	 * contains the start position (numIndex) and the number of documents to read
-	 * (numBlockSize).
-	 * <p>
-	 * The method updates the index for all affected documents which can be filtered
-	 * by 'type' and '$created'.
-	 * <p>
-	 * An existing lucene index must be deleted manually by the administrator.
+	 * This method runs the RebuildLuceneIndexJob. The job starts at creation date
+	 * 1970/01/01 and reads singel documents in sequence.
 	 * <p>
 	 * After the run method is finished, the properties numIndex, numUpdates and
 	 * numProcessed are updated.
 	 * <p>
-	 * If the number of documents returned from the DocumentService is less the the
-	 * BlockSize, the method returns true to indicate that the Timer should be
-	 * canceled.
+	 * 
 	 * 
 	 * @param adminp
-	 * @return true if no more unprocessed documents exist.
+	 * @return true when finished
 	 * @throws AccessDeniedException
 	 * @throws PluginException
 	 */
 	@Override
 	public ItemCollection run(ItemCollection adminp) throws AdminPException {
-
 		long lProfiler = System.currentTimeMillis();
-		int iIndex = adminp.getItemValueInteger("numIndex");
-		int iBlockSize = adminp.getItemValueInteger("numBlockSize");
+		long syncPoint = 0;
+		int totalCount = 0;
+		int blockCount = 0;
 
-		// test if numBlockSize is defined.
-		if (iBlockSize <= 0) {
-			// no set default block size.
-			iBlockSize = DEFAULT_BLOCK_SIZE;
-			adminp.replaceItemValue("numBlockSize", iBlockSize);
-		}
+		try {
+			while (true) {
+				List<ItemCollection> resultList = new ArrayList<ItemCollection>();
+				List<Document> documents = findNextDocumentsBySyncPoint(syncPoint);
 
-		int iUpdates = adminp.getItemValueInteger("numUpdates");
-		int iProcessed = adminp.getItemValueInteger("numProcessed");
+				if (documents != null && documents.size() > 0) {
+					for (Document doc : documents) {
+						// update syncpoint
+						syncPoint = doc.getCreated().getTimeInMillis();
+						resultList.add(new ItemCollection(doc.getData()));
+						// detach object!
+						manager.detach(doc);
+					}
 
-		// buidl query...
-		String query = buildQuery(adminp);
-		logger.finest("......JQPL query: " + query);
-		adminp.replaceItemValue("txtQuery", query);
-		// query documents by direct access to the EntityManager - skip read
-		// access verification here because we are still running with
-		// ACCESSLEVEL.MANAGERACCESS!
-		Query q = manager.createQuery(query);
-		q.setFirstResult(iIndex);
-		q.setMaxResults(iBlockSize);
-		@SuppressWarnings("unchecked")
-		Collection<Document> documentList = q.getResultList();
-		// collect ItemCollection elements
-		List<ItemCollection> col = new ArrayList<ItemCollection>();
-		for (Document doc : documentList) {
-			if (doc.getData() != null) {
-				col.add(new ItemCollection(doc.getData()));
+					// update the index
+					luceneUpdateService.updateDocumentsUncommitted(resultList);
+
+					// update count
+					totalCount += resultList.size();
+					blockCount += resultList.size();
+					if (blockCount >= BLOCK_SIZE) {
+						long time = (System.currentTimeMillis() - lProfiler) / 1000;
+						if (time == 0) {
+							time = 1;
+						}
+						logger.info("...Job " + AdminPService.JOB_REBUILD_LUCENE_INDEX + " (" + adminp.getUniqueID()
+								+ ") - ..." + totalCount + " documents indexed in " + time + " sec. ... ");
+						blockCount = 0;
+					}
+				} else {
+					// no more documents
+					break;
+				}
 			}
+		} catch (Exception e) {
+			// print exception and stop job
+			logger.severe("...Job " + AdminPService.JOB_REBUILD_LUCENE_INDEX + " (" + adminp.getUniqueID()
+					+ ") - failed - " + e.getMessage() + " last syncpoint  " + syncPoint + " - " + totalCount
+					+ "  documents reindexed....");
+			e.printStackTrace();
+			adminp.replaceItemValue(JobHandler.ISCOMPLETED, false);
+			// update syncpoint
+			Date syncDate = new Date(syncPoint);
+			adminp.replaceItemValue("error", e.getMessage());
+			adminp.replaceItemValue(ITEM_SYNCPOINT, syncPoint);
+			adminp.replaceItemValue(ITEM_SYNCDATE, syncDate);
+			adminp.replaceItemValue("numUpdates", totalCount);
+			adminp.replaceItemValue("numProcessed", totalCount);
+			adminp.replaceItemValue("numLastCount", 0);
+			return adminp;
 		}
 
-		int colSize = col.size();
-		// Update index
-		logger.info("Job " + AdminPService.JOB_REBUILD_LUCENE_INDEX + " (" + adminp.getUniqueID() + ") - reindexing "
-				+ col.size() + " documents...");
-
-		// write lucene index immediately (see issue #419)....
-		luceneUpdateService.updateDocumentsUncommitted(col);
-		
-		iUpdates = iUpdates + colSize;
-		iIndex = iIndex + col.size();
-		iProcessed = iProcessed + colSize;
-
-		// adjust start pos and update count for next run
-		adminp.replaceItemValue("numUpdates", iUpdates);
-		adminp.replaceItemValue("numProcessed", iProcessed);
-		adminp.replaceItemValue("numIndex", iIndex);
-
+		// completed
 		long time = (System.currentTimeMillis() - lProfiler) / 1000;
 		if (time == 0) {
 			time = 1;
 		}
+		logger.info("...Job " + AdminPService.JOB_REBUILD_LUCENE_INDEX + " (" + adminp.getUniqueID() + ") - Finished: "
+				+ totalCount + " documents indexed in " + time + " sec. ");
 
-		logger.info("...Job " + AdminPService.JOB_REBUILD_LUCENE_INDEX + " (" + adminp.getUniqueID() + ") - " + colSize
-				+ " documents reindexed in " + time + " sec.  (in total: " + iProcessed);
-
-		// if colSize<numBlockSize we can stop the timer
-		if (colSize < iBlockSize) {
-			// iscompleted = true
-			adminp.replaceItemValue(JobHandler.ISCOMPLETED, true);
-		}
-
+		adminp.replaceItemValue(JobHandler.ISCOMPLETED, true);
+		adminp.replaceItemValue("numUpdates", totalCount);
+		adminp.replaceItemValue("numProcessed", totalCount);
+		adminp.replaceItemValue("numLastCount", 0);
 		return adminp;
+
 	}
 
-	
 	/**
-	 * This method builds the query statemetn based on the filter criteria.
+	 * Loads the next documents by a given symcpoint (timestamp in milis) compared
+	 * with the created timestamp of a document entity.
+	 * <p>
+	 * It is possible that more than one document entities have the same created
+	 * timestamp. For that reason the method returns all documents with the same
+	 * timestamp in a collection.
 	 * 
-	 * @param adminp
-	 * @return
+	 * @param lSyncpoint
+	 * @return a list of documents with the same creation timestamp after the given
+	 *         syncpoint. Returns null in case no more documents were found.
 	 */
-	private String buildQuery(ItemCollection adminp) {
-		Date datFilterFrom = adminp.getItemValueDate("datfrom");
-		Date datFilterTo = adminp.getItemValueDate("datto");
-		String typeFilter = adminp.getItemValueString("typelist");
-		SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd");
+	@SuppressWarnings("unchecked")
+	private List<Document> findNextDocumentsBySyncPoint(long lSyncpoint) {
 
+		Date syncpoint = new Date(lSyncpoint);
+		// ISO date time format: '2016-08-25 01:23:46.0',
+		DateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 		String query = "SELECT document FROM Document AS document ";
-		// ignore lucene event log entries
-		query += "WHERE document.type NOT IN ('" + LuceneUpdateService.EVENTLOG_TYPE_ADD + "','"
-				+ LuceneUpdateService.EVENTLOG_TYPE_REMOVE + "') ";
-
-		// ignore imixs-archive snapshots
-		query += "AND document.type NOT LIKE 'snapshot%'";
-
-		if (typeFilter != null && !typeFilter.isEmpty()) {
-			// convert type list into comma separated list
-			List<String> typeList = Arrays.asList(typeFilter.split("\\s*,\\s*"));
-			String sType = "";
-			for (String aValue : typeList) {
-				sType += "'" + aValue.trim() + "',";
+		query += " WHERE document.created > '" + isoFormat.format(syncpoint) + "'";
+		query += " AND NOT document.type LIKE '" + SNAPSHOT_TYPE_PRAFIX + "%' ";
+		query += " ORDER BY document.created ASC";
+		Query q = manager.createQuery(query);
+		q.setFirstResult(0);
+		// we query 2 max results just to verify if there are duplicats (which should
+		// happen very seldom)
+		q.setMaxResults(2);
+		List<Document> documentList = q.getResultList();
+		if (documentList != null && documentList.size() > 0) {
+			Document firstDocument = null;
+			Document nextDocument = null;
+			Iterator<Document> iter = documentList.iterator();
+			if (iter.hasNext()) {
+				firstDocument = iter.next();
 			}
-			sType = sType.substring(0, sType.length() - 1);
-			query += " AND document.type IN(" + sType + ")";
-
+			if (iter.hasNext()) {
+				nextDocument = iter.next();
+			}
+			// now test if we have more than one document with the same timestamp
+			if (firstDocument != null && nextDocument != null
+					&& firstDocument.getCreated().equals(nextDocument.getCreated())) {
+				logger.finest("......there are more than one document with the same creation timestamp!");
+				// lets build a collection
+				syncpoint = new Date(firstDocument.getCreated().getTimeInMillis());
+				query = "SELECT document FROM Document AS document ";
+				query += " WHERE document.created = '" + isoFormat.format(syncpoint) + "'";
+				query += " AND NOT document.type LIKE '" + SNAPSHOT_TYPE_PRAFIX + "%' ";
+				query += " ORDER BY document.created ASC";
+				q = manager.createQuery(query);
+				q.setFirstResult(0);
+				q.setMaxResults(BLOCK_SIZE);
+				return q.getResultList();
+			} else {
+				// we found exactly one document
+				return documentList;
+			}
 		}
-
-		if (datFilterFrom != null) {
-			query += " AND document.created>='" + isoFormat.format(datFilterFrom) + "' ";
-		}
-
-		if (datFilterTo != null) {
-			query += " AND document.created<='" + isoFormat.format(datFilterTo) + "' ";
-		}
-
-		query += " ORDER BY document.created";
-
-		return query;
+		return null;
 	}
+
 }
