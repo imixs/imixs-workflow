dddc95a20568110465bd1838d3d91e94be026e61
Ralph Soika <ralph.soika@imixs.com>
2019-11-26 11:18:14 +0100

    implementation
    Issue #613

---
+++ imixs-workflow-core/src/main/java/org/imixs/workflow/util/JSONBuilder.java
@@ -0,0 +1,183 @@
+package org.imixs.workflow.util;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.imixs.workflow.ItemCollection;
+
+/**
+ * The JSONBuilder is an utility class to generate a typed json string from an
+ * Imixs ItemCollection.
+ * <p>
+ * The result can be converted back into a ItemCollection by using the
+ * JSONParser class.
+ * 
+ * @author rsoika
+ */
+public class JSONBuilder {
+
+	public static final String ISO8601DATEFORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
+
+	/**
+	 * This method builds a typed JSON output stream from a Imixs ItemCollection.
+	 * 
+	 * Example Output: <code>
+	 *  {
+		"item":[
+				{"name":"$isauthor","value":{"@type":"xs:boolean","$":"true"}},
+				{"name":"$readaccess","value":{"@type":"xs:string","$":"Anna"}},
+				{"name":"txtmessage","value":{"@type":"xs:string","$":"worklist"}},
+				{"name":"txtlog","value":[
+					{"@type":"xs:string","$":"A"},
+					{"@type":"xs:string","$":"B"},
+					{"@type":"xs:string","$":"C"}]
+				},
+				{"name":"$eventid","value":{"@type":"xs:int","$":"0"}}
+			]
+		}
+	 * </code>
+	 * 
+	 * @param worktiem
+	 *            - ItemCollection to be translated into JSON
+	 * @return a JSON string
+	 * @throws ParseException
+	 * @throws UnsupportedEncodingException
+	 */
+	@SuppressWarnings("unchecked")
+	public final static String build(final ItemCollection workitem)
+			throws ParseException, UnsupportedEncodingException {
+
+		StringBuffer out = new StringBuffer();
+		out.append("{\"item\":[");
+
+		// iterate over all items...
+		List<String> itemNames = workitem.getItemNames();
+
+		for (int i = 0; i < itemNames.size(); i++) {
+
+			String itemName = itemNames.get(i);
+			out.append("{\"name\":\"" + itemName + "\",\"value\":");
+			List<Object> values = workitem.getItemValue(itemName);
+			buildValues(values, out);
+
+			// add comma?
+			if (i < (itemNames.size() - 1)) {
+				out.append(",");
+			}
+		}
+		out.append("]}");
+
+		return out.toString();
+	}
+
+	/**
+	 * This helper method converts a value list into a json string
+	 * <p>
+	 * In case values contains more than one item the values are ordered into a json
+	 * array.
+	 * <p>
+	 * e.g.
+	 * <p>
+	 * <code>
+	 * {"name":"$isauthor","value":{"@type":"xs:boolean","$":true}},
+	 * {"name":"$readaccess","value":{"@type":"xs:string","$":"Anna"}},
+	 * {"name":"txtmessage","value":{"@type":"xs:string","$":"worklist"}},
+	 * {"name":"$activityid","value":{"@type":"xs:int","$":10}},
+	 * {"name":"$processid","value":{"@type":"xs:int","$":100}}
+	 *  </code>
+	 * 
+	 * @param token
+	 * @throws ParseException
+	 */
+	private static void buildValues(List<Object> values, StringBuffer out) {
+		if (values == null || values.size() == 0) {
+			out.append("{}");
+			return;
+		}
+		if (values.size() > 1) {
+			out.append("[");
+		}
+
+		// print each output...
+		for (int i = 0; i < values.size(); i++) {
+			out.append("{");
+			// {"@type":"xs:string","$":"worklist"}
+			Object valueObject = values.get(i);
+			String type = "";
+			// test raw types first
+			if (valueObject instanceof String) {
+				type = "\"@type\":\"xs:string\"";
+			}
+			if (valueObject instanceof Boolean) {
+				type = "\"@type\":\"xs:boolean\"";
+			}
+			if (valueObject instanceof Short) {
+				type = "\"@type\":\"xs:short\"";
+			}
+			if (valueObject instanceof Integer) {
+				type = "\"@type\":\"xs:int\"";
+			}
+			if (valueObject instanceof Long) {
+				type = "\"@type\":\"xs:long\"";
+			}
+			if (valueObject instanceof Float) {
+				type = "\"@type\":\"xs:floatt\"";
+			}
+			if (valueObject instanceof Double) {
+				type = "\"@type\":\"xs:double\"";
+			}
+			if (valueObject instanceof Date || valueObject instanceof Calendar) {
+				type = "\"@type\":\"xs:dateTime\"";
+			}
+			if (valueObject instanceof BigInteger) {
+				type = "\"@type\":\"xs:integer\"";
+			}
+			if (valueObject instanceof BigDecimal) {
+				type = "\"@type\":\"xs:decimal\"";
+			}
+
+			out.append(type+",");
+
+			// print the value...
+			// "$":"worklist"}
+			String valueString = null;
+			if (valueObject instanceof Date || valueObject instanceof Calendar) {
+				// convert 2013-10-07T22:18:55.476+02:00
+				Date date = null;
+				if (valueObject instanceof Calendar) {
+					date = ((Calendar) valueObject).getTime();
+				} else {
+					date = (Date) valueObject;
+				}
+				SimpleDateFormat sdf = new SimpleDateFormat(ISO8601DATEFORMAT);
+				// sdf.setTimeZone(TimeZone.getTimeZone("CET"));
+				valueString = sdf.format(date);
+			} else {
+				// simple convert to string
+				valueString = valueObject.toString();
+			}
+
+			out.append("\"$\":\"" + valueString + "\"");
+
+			out.append("}");
+			// add comma?
+			if ((i) < (values.size() - 1)) {
+				out.append(",");
+			}
+		}
+
+		if (values.size() > 1) {
+			out.append("]");
+
+		}
+
+		out.append("}");
+	}
+
+}
---
+++ imixs-workflow-core/src/test/java/org/imixs/workflow/util/TestJSONBuilder.java
@@ -0,0 +1,100 @@
+package org.imixs.workflow.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.imixs.workflow.ItemCollection;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+/**
+ * Test class test the Imixs JSONBuilder class to build a JSON String form an
+ * ItemCollection.
+ * 
+ * 
+ * @author rsoika
+ */
+public class TestJSONBuilder {
+	private static Logger logger = Logger.getLogger(TestJSONBuilder.class.getName());
+
+	/**
+	 * Expected Output: <code>
+	 *  {
+		"item":[
+				{"name":"$isauthor","value":{"@type":"xs:boolean","$":"true"}},
+				{"name":"$readaccess","value":{"@type":"xs:string","$":"Anna"}},
+				{"name":"count","value":{"@type":"xs:int","$":"42"}},
+				{"name":"amount","value":{"@type":"xs:double","$":"100.99"}},
+				{"name":"txtlog","value":[
+					{"@type":"xs:string","$":"A"},
+					{"@type":"xs:string","$":"B"},
+					{"@type":"xs:string","$":"C"}]
+				},
+				{"name":"$eventid","value":{"@type":"xs:int","$":"0"}}
+			]
+		}
+	 * </code>
+	 * 
+	 * @throws ParseException
+	 */
+	@SuppressWarnings("rawtypes")
+	@Test
+	public void testSimple() throws ParseException {
+
+		ItemCollection workitem = new ItemCollection();
+		workitem.setItemValue("$isauthor", true);
+		workitem.setItemValue("$readaccess", "Anna");
+
+		workitem.setItemValue("count", 42);
+		workitem.setItemValue("amount", new Double(100.99));
+		workitem.appendItemValue("txtlog", "A");
+		workitem.appendItemValue("txtlog", "B");
+		workitem.appendItemValue("txtlog", "C");
+
+		String jsonResult = null;
+		try {
+			jsonResult = JSONBuilder.build(workitem);
+			Assert.assertNotNull(jsonResult);
+		} catch (UnsupportedEncodingException e) {
+
+			e.printStackTrace();
+			Assert.fail();
+		}
+
+		// now convert the json string back using the Imixs JSONParser
+		InputStream jsonStream = new ByteArrayInputStream(jsonResult.getBytes());
+		try {
+			ItemCollection testItemCol = JSONParser.parseWorkitem(jsonStream, "UTF-8");
+			Assert.assertNotNull(testItemCol);
+
+			// convert data....
+			Assert.assertEquals(workitem.getItemValueBoolean("$isauthor"),
+					testItemCol.getItemValueBoolean("$isauthor"));
+			Assert.assertEquals(workitem.getItemValueString("$readaccess"),
+					testItemCol.getItemValueString("$readaccess"));
+
+			List valueList = testItemCol.getItemValue("count");
+			Assert.assertTrue(valueList.get(0) instanceof Integer);
+			Assert.assertEquals(workitem.getItemValueInteger("count"), testItemCol.getItemValueInteger("count"));
+			Assert.assertEquals(workitem.getItemValueDouble("amount"), testItemCol.getItemValueDouble("amount"));
+
+			// test value list
+			valueList = testItemCol.getItemValue("txtlog");
+			Assert.assertEquals(3, valueList.size());
+			Assert.assertEquals("C", valueList.get(2));
+
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+
+		logger.info(jsonResult);
+
+	}
+
+}
