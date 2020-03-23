6c56bfb8fe0769252bebff075c97e4d3a7817652
Ralph Soika <ralph.soika@imixs.com>
2020-03-23 12:25:35 +0100

    fixed
    Issue #654

--- imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/UserGroupEvent.java
+++ imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/UserGroupEvent.java
@@ -28,26 +28,30 @@
 
 package org.imixs.workflow.engine;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * The UserGroupEvent provides a CDI observer pattern. The UserGroupEvent is
  * fired by the DocumentService EJB. An event Observer can react on this event
- * to extend the current user group list.
+ * to extend the current user group list. The user group list is used to grant
+ * read and write access on a document entity.
  * 
  * 
  * @author Ralph Soika
- * @version 1.0
+ * @version 1.1
  * @see org.imixs.workflow.engine.DocumentService
  */
 public class UserGroupEvent {
 
     private String userId;
-    private List<String> groups;
+    private Set<String> groups;
 
     public UserGroupEvent(String userId) {
         super();
         this.userId = userId;
+        this.groups = new LinkedHashSet<String>();
     }
 
     public String getUserId() {
@@ -58,12 +62,33 @@ public class UserGroupEvent {
         this.userId = userId;
     }
 
+    /**
+     * This method adds a new list of groups to the current group list. Multiple
+     * observers can add different groups independently from each other. Duplicates
+     * are removed.
+     * <p>
+     * To reset the current group list a observer can call the method reset.
+     * 
+     * @param groups
+     */
     public void setGroups(List<String> groups) {
-        this.groups = groups;
+        // see issue #654
+        this.groups.addAll(groups);
     }
 
-    public List<String> getGroups() {
+    /**
+     * Returns the current list of groups
+     * 
+     * @return
+     */
+    public Set<String> getGroups() {
         return groups;
     }
 
+    /**
+     * This method empties the current group list
+     */
+    public void reset() {
+        this.groups = new LinkedHashSet<String>();
+    }
 }
