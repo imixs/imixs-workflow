6184a486f1a6459a5967ff98d9c80db54ec2c908
Ralph Soika <ralph.soika@imixs.com>
2020-06-19 13:13:40 +0200

    implementation and docu
    Issue #686

--- imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/MetricService.java
+++ imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/MetricService.java
@@ -28,11 +28,15 @@
 
 package org.imixs.workflow.engine;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.ObserverException;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.metrics.Counter;
 import org.eclipse.microprofile.metrics.Metadata;
@@ -71,155 +75,182 @@ import org.imixs.workflow.exceptions.AccessDeniedException;
 @ApplicationScoped
 public class MetricService {
 
-    public static final String METRIC_DOCUMENTS = "documents";
-    public static final String METRIC_WORKITEMS = "workitems";
+	public static final String METRIC_DOCUMENTS = "documents";
+	public static final String METRIC_WORKITEMS = "workitems";
+	public static final String METRIC_TRANSACTIONS = "transactions";
 
-    @Inject
-    @ConfigProperty(name = "metrics.enabled", defaultValue = "false")
-    private boolean metricsEnabled;
+	@Inject
+	@ConfigProperty(name = "metrics.enabled", defaultValue = "false")
+	private boolean metricsEnabled;
 
-    @Inject
-    @RegistryType(type = MetricRegistry.Type.APPLICATION)
-    MetricRegistry metricRegistry;
+	@Inject
+	@ConfigProperty(name = "metrics.anonymised", defaultValue = "true")
+	private boolean metricsAnonymised;
 
-    boolean mpMetricNoSupport = false;
+	@Inject
+	@RegistryType(type = MetricRegistry.Type.APPLICATION)
+	MetricRegistry metricRegistry;
 
-    private static Logger logger = Logger.getLogger(MetricService.class.getName());
+	boolean mpMetricNoSupport = false;
 
-    /**
-     * ProcessingEvent listener to generate a metric.
-     * 
-     * @param processingEvent
-     * @throws AccessDeniedException
-     */
-    public void onProcessingEvent(@Observes ProcessingEvent processingEvent) throws AccessDeniedException {
+	private static Logger logger = Logger.getLogger(MetricService.class.getName());
 
-        if (!metricsEnabled) {
-            return;
-        }
-        if (processingEvent == null) {
-            return;
-        }
-        if (mpMetricNoSupport) {
-            // missing MP Metric support!
-            return;
-        }
+	/**
+	 * ProcessingEvent listener to generate a metric.
+	 * 
+	 * @param processingEvent
+	 * @throws AccessDeniedException
+	 */
+	public void onProcessingEvent(@Observes ProcessingEvent processingEvent) throws AccessDeniedException {
 
-        // NOTE: Issue #514 - just uncomment this code!
-        try {
-            Counter counter = buildWorkitemMetric(processingEvent);
-            counter.inc();
-        } catch (IncompatibleClassChangeError | ObserverException oe) {
-            mpMetricNoSupport = true;
-            logger.warning("...Microprofile Metrics v2.2 not supported!");
-        }
-    }
+		if (!metricsEnabled) {
+			return;
+		}
+		if (processingEvent == null) {
+			return;
+		}
+		if (mpMetricNoSupport) {
+			// missing MP Metric support!
+			return;
+		}
 
-    /**
-     * DocumentEvent listener to generate a metric.
-     * 
-     * @param documentEvent
-     * @throws AccessDeniedException
-     */
-    public void onDocumentEvent(@Observes DocumentEvent documentEvent) throws AccessDeniedException {
+		// NOTE: Issue #514 - just uncomment this code!
+		try {
+			Counter counter = buildWorkitemMetric(processingEvent);
+			counter.inc();
+		} catch (IncompatibleClassChangeError | ObserverException oe) {
+			mpMetricNoSupport = true;
+			logger.warning("...Microprofile Metrics v2.2 not supported!");
+		}
+	}
 
-        if (!metricsEnabled) {
-            return;
-        }
-        if (documentEvent == null) {
-            return;
-        }
-        if (mpMetricNoSupport) {
-            // missing MP Metric support!
-            return;
-        }
+	/**
+	 * DocumentEvent listener to generate a metric.
+	 * 
+	 * @param documentEvent
+	 * @throws AccessDeniedException
+	 */
+	public void onDocumentEvent(@Observes DocumentEvent documentEvent) throws AccessDeniedException {
 
-        // NOTE: Issue #514 - just uncomment this code!
-        try {
-            Counter counter = buildDocumentMetric(documentEvent);
-            counter.inc();
+		if (!metricsEnabled) {
+			return;
+		}
+		if (documentEvent == null) {
+			return;
+		}
+		if (mpMetricNoSupport) {
+			// missing MP Metric support!
+			return;
+		}
 
-        } catch (IncompatibleClassChangeError | ObserverException oe) {
-            mpMetricNoSupport = true;
-            logger.warning("...Microprofile Metrics v2.0 not supported!");
-            oe.printStackTrace();
-        }
-    }
+		// NOTE: Issue #514 - just uncomment this code!
+		try {
+			Counter counter = buildDocumentMetric(documentEvent);
+			counter.inc();
 
-    /**
-     * This method builds a Microprofile Metric for a Counter. The metric contains
-     * the tag 'method'.
-     * 
-     * @return Counter metric
-     */
+		} catch (IncompatibleClassChangeError | ObserverException oe) {
+			mpMetricNoSupport = true;
+			logger.warning("...Microprofile Metrics v2.0 not supported!");
+			oe.printStackTrace();
+		}
+	}
 
-    // NOTE: Issue #514 - just uncomment this code!
+	/**
+	 * This method builds a Microprofile Metric for a Counter. The metric contains
+	 * the tag 'method'.
+	 * 
+	 * @return Counter metric
+	 */
 
-    private Counter buildDocumentMetric(DocumentEvent event) {
+	// NOTE: Issue #514 - just uncomment this code!
 
-        // Constructs a Metadata object from a map with the following keys:
-        // - name - The name of the metric
-        // - displayName - The display (friendly) name of the metric
-        // - description - The description of the metric
-        // - type - The type of the metric
-        // - tags - The tags of the metric - cannot be null
-        // - reusable - If true, this metric name is permitted to be used at multiple
+	private Counter buildDocumentMetric(DocumentEvent event) {
 
-        Metadata metadata = Metadata.builder().withName(METRIC_DOCUMENTS)
-                .withDescription("Imixs-Workflow count documents").withType(MetricType.COUNTER).build();
+		// Constructs a Metadata object from a map with the following keys:
+		// - name - The name of the metric
+		// - displayName - The display (friendly) name of the metric
+		// - description - The description of the metric
+		// - type - The type of the metric
+		// - tags - The tags of the metric - cannot be null
+		// - reusable - If true, this metric name is permitted to be used at multiple
 
-        String method = null;
-        // build tags...
-        if (DocumentEvent.ON_DOCUMENT_SAVE == event.getEventType()) {
-            method = "save";
-        }
+		Metadata metadata = Metadata.builder().withName(METRIC_DOCUMENTS)
+				.withDescription("Imixs-Workflow count documents").withType(MetricType.COUNTER).build();
 
-        if (DocumentEvent.ON_DOCUMENT_LOAD == event.getEventType()) {
-            method = "load";
-        }
+		String method = null;
+		// build tags...
+		if (DocumentEvent.ON_DOCUMENT_SAVE == event.getEventType()) {
+			method = "save";
+		}
 
-        if (DocumentEvent.ON_DOCUMENT_DELETE == event.getEventType()) {
-            method = "delete";
-        }
+		if (DocumentEvent.ON_DOCUMENT_LOAD == event.getEventType()) {
+			method = "load";
+		}
 
-        Tag[] tags = { new Tag("method", method) };
+		if (DocumentEvent.ON_DOCUMENT_DELETE == event.getEventType()) {
+			method = "delete";
+		}
 
-        Counter counter = metricRegistry.counter(metadata, tags);
+		Tag[] tags = { new Tag("method", method) };
 
-        return counter;
-    }
+		Counter counter = metricRegistry.counter(metadata, tags);
 
-    /**
-     * This method builds a Microprofile Metric for a Counter. The metric contains
-     * the tags 'task', 'event', 'type', 'workflowgroup', 'worklowstatus',
-     * 'modelversion'
-     * 
-     * @return Counter metric
-     */
-    private Counter buildWorkitemMetric(ProcessingEvent event) {
+		return counter;
+	}
 
-        // Constructs a Metadata object from a map with the following keys:
-        // - name - The name of the metric
-        // - displayName - The display (friendly) name of the metric
-        // - description - The description of the metric
-        // - type - The type of the metric
-        // - tags - The tags of the metric - cannot be null
-        // - reusable - If true, this metric name is permitted to be used at multiple
+	/**
+	 * This method builds a Microprofile Metric for a Counter. The metric contains
+	 * the tags 'task', 'event', 'type', 'workflowgroup', 'worklowstatus',
+	 * 'modelversion'
+	 * 
+	 * @return Counter metric
+	 */
+	private Counter buildWorkitemMetric(ProcessingEvent event) {
 
-        Metadata metadata = Metadata.builder().withName(METRIC_WORKITEMS)
-                .withDescription("Imixs-Workflow count procssed workitems").withType(MetricType.COUNTER).build();
+		// Constructs a Metadata object from a map with the following keys:
+		// - name - The name of the metric
+		// - displayName - The display (friendly) name of the metric
+		// - description - The description of the metric
+		// - type - The type of the metric
+		// - tags - The tags of the metric - cannot be null
+		// - reusable - If true, this metric name is permitted to be used at multiple
 
-        // build tags...
-        Tag[] tags = { new Tag("type", event.getDocument().getType()),
-                new Tag("modelversion", event.getDocument().getModelVersion()),
-                new Tag("task", event.getDocument().getTaskID() + ""),
-                new Tag("event", event.getDocument().getItemValueInteger("$lastevent") + ""),
-                new Tag("workflowgroup", event.getDocument().getItemValueString(WorkflowKernel.WORKFLOWGROUP)),
-                new Tag("workflowstatus", event.getDocument().getItemValueString(WorkflowKernel.WORKFLOWSTATUS)) };
+		
 
-        Counter counter = metricRegistry.counter(metadata, tags);
+		// BEFORE_PROCESS
+		if (event.getEventType() == ProcessingEvent.BEFORE_PROCESS) {
+			// only transaction count - independent form the result
+			Metadata metadata = Metadata.builder().withName(METRIC_TRANSACTIONS)
+					.withDescription("Imixs-Workflow transactions").withType(MetricType.COUNTER).build();
+			Counter counter = metricRegistry.counter(metadata);
+			return counter;
 
-        return counter;
-    }
+		} else {
+			// AFTER_PROCESS
+			// build workflow tags...
+			List<Tag> tags = new ArrayList<Tag>();
+			tags.add(new Tag("type", event.getDocument().getType()));
+			tags.add(new Tag("modelversion", event.getDocument().getModelVersion()));
+			tags.add(new Tag("task", event.getDocument().getTaskID() + ""));
+			tags.add(new Tag("workflowgroup", event.getDocument().getItemValueString(WorkflowKernel.WORKFLOWGROUP)));
+			tags.add(new Tag("workflowstatus", event.getDocument().getItemValueString(WorkflowKernel.WORKFLOWSTATUS)));
+
+			// metricsAnonymised = false?
+			if (!metricsAnonymised) {
+				// add the user id....
+				String user = event.getDocument().getItemValueString(WorkflowKernel.EDITOR);
+				tags.add(new Tag("user", user));
+			}
+			tags.add(new Tag("event", event.getDocument().getItemValueInteger("$lastevent") + ""));
+			Metadata metadata = Metadata.builder().withName(METRIC_WORKITEMS)
+					.withDescription("Imixs-Workflow count processed workitems").withType(MetricType.COUNTER).build();
+			Tag[] tagArr = tags.toArray(new Tag[tags.size()]);
+			Counter counter = metricRegistry.counter(metadata, tagArr);
+
+			return counter;
+
+		}
+
+	}
 
 }
--- src/site/markdown/engine/metrics.md
+++ src/site/markdown/engine/metrics.md
@@ -4,25 +4,103 @@ Imixs-Workflow provides runtime metrics based on the [Microprofile Metric API](h
 
 The Imixs-Metric Serivce is disabled per default. To enable the feature set the property 'metrics.enabled' to 'true.
 
-
 	metrics.enabled=true
 
 ## How to access Imixs-Workflow Metrics
 
-The Imxis-Workflow metrics can be collected by the following Rest Service resource:
+The Imxis-Workflow metrics can be collected by the metric rest endpoint depending on the application server.
 
-	http://...../metrics
+    # Wildfly
+    http://[HOST]:9990/metrics
+    # Payara
+    http://[HOST]:8080/metrics
+    
 
 This is an example how a Imixs-Workflow metric looks like:
+	application_workitems_total{event="20",modelversion="invoice-1.2",task="2001",type="workitem",workflowgroup="Invoice",workflowstatus="Open"} 1.0
+	application_workitems_total{event="1",modelversion="todo-1.0.0",task="2000",type="workitem",workflowgroup="ToDo",workflowstatus="Offen"} 3.0
+	application_documents_total{method="save"} 8.0
+	application_documents_total{method="delete"} 3.0
+	application_documents_total{method="load"} 133.0
+	application_transactions_total 5.0
 
-	# HELP documents_total The total number of accessed documents.
-	# TYPE documents_total counter
-	documents_loaded_total{method="load"} 98
-	documents_deleted_total{method="delete"} 3
-	documents_saved_total{method="save"} 6
-	# HELP workitems_processed_total The total number of workitems processeds.
-	# TYPE workitems_processed_total counter
-	workitems_processed_total{type="workitem",modelversion="protokoll-de-2.0.0",task="1100",event="10",workflowgroup="Protokoll",workflowstatus="Erstellung"}  1
-	workitems_processed_total{type="workitem",modelversion="protokoll-de-2.0.0",task="1500",event="100",workflowgroup="Protokoll",workflowstatus="Freigegeben"}  2
-	
+
 The Document and Workflow metrics are shown here. Each metric provides a set of tags defining the method and additional metadata like the WorklfowVersion or the current workflow event processed by the Imixs-Workflow Engine.
+
+
+## Anonymised Metrics
+
+The metrics provided by the Imixs workflow platform are anonymized by default. This is important in order to comply with the data protection directives of many countries as well as within the European Union. But in some cases, it may be necessary to provide personalized metrics. For this purpose you can set the flag '_metrics.anonymised_' to 'false' (default = true). In this case, the metrics are supplemented with the  tag 'user' providing the corresponding userid. 
+
+	METRICS_ENABLED: "true"
+	METRICS_ANONYMISED: "false"
+
+
+
+## Metrics Endpoint
+
+The Metrics are based on the [Eclipse Microprofile Metric API](https://microprofile.io/project/eclipse/microprofile-metrics) which means that the metric api is part of your application server. Depending on your platform you will get a lot of hardware and environment metrics together with the Imixs-Workflow metrics. The endpoint to access the metrics depends on your server environment. 
+
+### Prometheus Configuration for Widlfly
+
+The default metric api endpoint for Wildfly is:
+
+	https://[HOST]:9990/metrics
+	
+A Prometheus configuration to scrape the metrics from Imixs-Workflow can look like in the following example:
+
+	global:
+	  scrape_interval:     15s # By default, scrape targets every 15 seconds
+	  # Attach these labels to any time series or alerts when communicating with
+	  # external systems (federation, remote storage, Alertmanager).
+	  external_labels:
+	    monitor: 'imixs-monitor'
+	
+	scrape_configs:
+	  # Prometheus itself
+	  - job_name: 'prometheus'
+	    scrape_interval: 5s
+	    static_configs:
+	      - targets: ['localhost:9090']
+	     
+	  # Imixs-Office Job
+	  - job_name: 'imixs'
+	    scrape_interval: 5s
+	    metrics_path: /metrics
+	    static_configs:
+	      - targets: ['app:9990']      
+   
+
+
+### Prometheus Configuration for Payara
+
+The default metric api endpoint for Payara Server is:
+
+	https://[HOST]:8080/metrics
+	
+A Prometheus configuration to scrape the metrics from Imixs-Workflow can look like in the following example:
+
+
+	global:
+	  scrape_interval:     15s # By default, scrape targets every 15 seconds.
+	  # Attach these labels to any time series or alerts when communicating with
+	  # external systems (federation, remote storage, Alertmanager).
+	  external_labels:
+	    monitor: 'imixs-monitor'
+	
+	scrape_configs:
+	  # Prometheus itself
+	  - job_name: 'prometheus'
+	    scrape_interval: 5s
+	    static_configs:
+	      - targets: ['localhost:9090']
+	     
+	  # Imixs-Office Job
+	  - job_name: 'imixs'
+	    scrape_interval: 5s
+	    metrics_path: /metrics
+	    static_configs:
+	      - targets: ['app:8080']        
+ 
+
+      
