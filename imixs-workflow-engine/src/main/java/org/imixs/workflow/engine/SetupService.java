a17f56f86308337ee708bb4a3dadc16ea725e394
Ralph Soika <ralph.soika@imixs.com>
2021-07-05 16:15:17 +0200

    logging

--- imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/SetupService.java
+++ imixs-workflow-engine/src/main/java/org/imixs/workflow/engine/SetupService.java
@@ -148,7 +148,7 @@ public class SetupService {
         logger.info("   ____      _");
         logger.info("  /  _/_ _  (_)_ __ ___   Workflow");
         logger.info(" _/ //  ' \\/ /\\ \\ /(_-<   Engine");
-        logger.info("/___/_/_/_/_//_\\_\\/___/   V5.2");
+        logger.info("/___/_/_/_/_//_\\_\\/___/   V6.0");
         logger.info("");
 
         logger.info("...initalizing models...");
