2c8bc651db86e2020f4a087479fdb815a2bbe25f
Ralph Soika <ralph.soika@imixs.com>
2025-03-13 21:03:38 +0100

    fixed typo

--- imixs-workflow-jax-rs/src/main/java/org/imixs/workflow/jaxrs/RestAPIUtil.java
+++ imixs-workflow-jax-rs/src/main/java/org/imixs/workflow/jaxrs/RestAPIUtil.java
@@ -1,16 +1,15 @@
 package org.imixs.workflow.jaxrs;
 
 import java.util.List;
+import java.util.StringTokenizer;
 import java.util.Vector;
-
-import com.ibm.icu.util.StringTokenizer;
 
 /**
  * This Utility class provides methods to convert Item Values in Rest API calls.
  */
 public class RestAPIUtil {
-    
-     /**
+
+    /**
      * This method returns a List object from a given comma separated string. The
      * method returns null if no elements are found. The provided parameter looks
      * typical like this: <code>
